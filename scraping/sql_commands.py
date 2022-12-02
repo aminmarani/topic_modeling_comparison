46cc77ba2c889d82854c72b275ca23cd822269a0
amin <aminhosseiny@gmail.com>
2022-12-02 11:11:50 -0500

    =

--- scraping/sql_commands.py
+++ scraping/sql_commands.py
@@ -2,19 +2,21 @@ import mysql.connector
 
 
 
-def insert_platform(sql_cursor,platform_id,platform_name):
+def insert_platform(sql_connector,platform_id,platform_name):
     '''
     Insert a unique platform (id,name) into platform_type table
     
-    parameter sql_cursor: mysql_connector.cursor object
+    parameter sql_connector: mysql_connector.connect object
     parameter platform_id: id for the platform
     parameter platform_name: name of the platform (e.g., Tumblr)
     '''
-    cmd_str = ("INSERT INTO platfrom_type"
+    cmd_str = ("INSERT INTO platform_type"
                "(platform_type_name) "
                "VALUES (%s)")
-    res = sql_cursor.execute(cmd_str, (platform_name,))
-    cnx.commit()
-    cnx.close()
+    
+    cursor = sql_connector.cursor()
+    res = cursor.execute(cmd_str, (platform_name,))
+    sql_connector.commit()
+    sql_connector.close()
 
     return res
